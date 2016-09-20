e9c81ece9192c6578f4e05c56f60e806f87abdef
huangziyi01 <huangziyi01@baidu.com>
2016-09-20 20:14:51 +0800

    发布组件

---
+++ lib/gaea/gaea.type.d.ts
@@ -0,0 +1,13 @@
+export interface PropsDefine extends CommonModel.TransmitTransparentlyProps {
+}
+export declare class PropsGaea {
+    gaeaName: string;
+    gaeaIcon: string;
+    gaeaUniqueKey: string;
+}
+export declare class Props extends PropsGaea implements PropsDefine {
+}
+export interface StateDefine {
+}
+export declare class State implements StateDefine {
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,4 @@
+import Gaea from './gaea/gaea.component';
+import { PropsDefine as GaeaPropsDefine } from './gaea/gaea.type';
+export { Gaea, GaeaPropsDefine };
+export default Gaea;
