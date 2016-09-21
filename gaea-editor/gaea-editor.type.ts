8abfe21f5c6af683553ed3e4cf7bcf4c875a3b9b
huangziyi01 <huangziyi01@baidu.com>
2016-09-21 09:38:55 +0800

    开发

--- gaea-editor/gaea-editor.type.ts
+++ gaea-editor/gaea-editor.type.ts
@@ -1,6 +1,7 @@
 import * as React from 'react'
+import {TransparentlyPropsPropsDefine} from '../../../common/transparently-props/index'
 
-export interface PropsDefine extends CommonModel.TransmitTransparentlyProps {
+export interface PropsDefine extends TransparentlyPropsPropsDefine {
 
 }
 
