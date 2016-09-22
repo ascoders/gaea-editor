938adc9acb5722e8929315268a5ea9fbe79d01d6
huangziyi01 <huangziyi01@baidu.com>
2016-09-22 09:43:53 +0800

    开发

--- demo/1.web.tsx
+++ demo/1.web.tsx
@@ -2,16 +2,23 @@ import * as React from 'react'
 import {observer} from 'mobx-react'
 import Gaea from '../index'
 import webBaseComponents from '../../gaea-web-components/index'
+import Message from '../../../web-common/message/index'
 
 @observer
 export default class Demo extends React.Component <any, any> {
     static title = 'Web 编辑器'
     static description = ``
 
+    handleSave(obj: any) {
+        Message.info(JSON.stringify(obj))
+        console.log(JSON.stringify(obj))
+    }
+
     render() {
         return (
             <div style={{border: '1px solid #eee'}}>
-                <Gaea baseComponents={webBaseComponents}/>
+                <Gaea baseComponents={webBaseComponents}
+                      onSave={this.handleSave.bind(this)}/>
             </div>
         )
     }
