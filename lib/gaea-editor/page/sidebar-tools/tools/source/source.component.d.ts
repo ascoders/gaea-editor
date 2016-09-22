6a9281824db69e65c3cf2a685c2188ca63a0e031
huangziyi01 <huangziyi01@baidu.com>
2016-09-22 15:20:40 +0800

    发布组件

--- lib/gaea-editor/page/sidebar-tools/tools/source/source.component.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as typings from './source.type';
-export default class Source extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    render(): JSX.Element;
-}
--- lib/gaea-editor/store/setting.d.ts
+++
@@ -1,6 +0,0 @@
-export default class Setting {
-    confirmWhenRemoveComponent: boolean;
-    setConfirmWhenRemoveComponent(isConfirm: boolean): void;
-    showLayoutWhenDragging: boolean;
-    setShowLayoutWhenDragging(isShow: boolean): void;
-}
