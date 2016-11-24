7942f609d18f25ea38c15c3c066b0624e1701935
huangziyi01 <huangziyi01@baidu.com>
2016-11-24 14:19:45 +0800

    删除lib

--- lib/gaea-editor/components/editor-tabs-event/event-components/update-props/update-props.component.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as typings from './update-props.type';
-import './update-props.scss';
-export default class UpdatePropsEvent extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    handleClick(): void;
-    render(): JSX.Element;
-}
--- lib/gaea-editor/components/external-variable-editor/action.d.ts
+++
@@ -1,7 +0,0 @@
-export default class ExternalVariableEditorAction {
-    private externalVariableEditorStore;
-    private ViewportStore;
-    observableClass: boolean;
-    setCurrentEditComponentVariableByField(field: string, variable: FitGaea.VariableData): void;
-    removeCurrentEditComponentVariableByField(field: string): void;
-}
