054c43b092cfc490fd634031a00816964a3037b9
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 15:16:57 +0800

    发布组件

--- lib/gaea-editor/components/tab-tools-version/action.d.ts
+++
@@ -1,8 +0,0 @@
-export default class TabToolsVersionAction {
-    private tabToolsVersion;
-    observeClass: boolean;
-    setCurrentVersionPage(page: number): void;
-    addVersions(versions: Array<FitGaea.GetPublishListResult>): void;
-    setCurrentVersion(version: string): void;
-    publishToVersionList(versionInfo: FitGaea.GetPublishListResult): void;
-}
