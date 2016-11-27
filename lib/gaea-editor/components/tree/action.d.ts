b926ec5cef7b0a7f9730616b7e8e18d8d11f32ea
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 18:14:37 +0800

    发布组件

--- lib/gaea-editor/components/tree/action.d.ts
+++
@@ -1,7 +0,0 @@
-export default class TreeAction {
-    private tree;
-    observeClass: boolean;
-    setTreeRootDom(dom: HTMLElement): void;
-    addTreeDom(mapUniqueKey: string, dom: HTMLElement): void;
-    removeTreeDom(mapUniqueKey: string): void;
-}
