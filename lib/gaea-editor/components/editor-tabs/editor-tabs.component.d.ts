4f0f0c45d79b264e0c994561d1364bda8f96934a
huangziyi01 <huangziyi01@baidu.com>
2016-11-27 15:24:14 +0800

    发布组件

--- lib/gaea-editor/components/editor-tabs/editor-tabs.component.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as typings from './editor-tabs.type';
-import './editor-tabs.scss';
-export default class EditorTabs extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    static position: string;
-    render(): JSX.Element;
-}
--- lib/gaea-editor/components/publish/store.d.ts
+++
@@ -1,4 +0,0 @@
-export default class PublishStore {
-    private application;
-    currentVersion: string;
-}
