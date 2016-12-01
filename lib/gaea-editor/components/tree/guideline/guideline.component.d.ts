4c6e29905b01b00b0d99f95769a3a09db31b7abd
huangziyi01 <huangziyi01@baidu.com>
2016-12-01 23:27:15 +0800

    发布组件

--- lib/gaea-editor/components/tree/guideline/guideline.component.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as typings from './guideline.type';
-import './guideline.scss';
-export default class Guideline extends React.Component<typings.PropsDefine, typings.StateDefine> {
-    static defaultProps: typings.PropsDefine;
-    state: typings.StateDefine;
-    componentWillReact(): void;
-    render(): JSX.Element;
-}
--- lib/gaea-editor/utils/dom.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function hasClass(obj: HTMLElement, cls: string): RegExpMatchArray;
-export declare function removeClass(obj: HTMLElement, cls: string): void;
