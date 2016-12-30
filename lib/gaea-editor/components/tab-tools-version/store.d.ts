6b6c66421c89a86010273b5013b8c00b484b2cd8
huangziyi01 <huangziyi01@baidu.com>
2016-12-30 11:56:30 +0800

    发布组件

--- lib/gaea-editor/components/tab-tools-version/store.d.ts
+++
@@ -1,5 +0,0 @@
-export default class TabToolsVersionStore {
-    currentVersionPage: number;
-    versionList: Array<FitGaea.GetPublishListResult>;
-    currentVersion: string;
-}
--- lib/gaea-editor/components/tree/store.d.ts
+++
@@ -1,6 +0,0 @@
-export default class TreeStore {
-    private viewport;
-    treeRootDom: HTMLElement;
-    treeDoms: Map<string, HTMLElement>;
-    readonly currentHoverTreeDom: HTMLElement;
-}
