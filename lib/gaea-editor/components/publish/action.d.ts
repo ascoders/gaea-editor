6293b24fd70f429e7d35d82f59a5f44a60db0b29
huangziyi01 <huangziyi01@baidu.com>
2016-12-30 12:00:30 +0800

    发布组件

--- lib/gaea-editor/components/publish/action.d.ts
+++
@@ -1,7 +0,0 @@
-export default class PublishAction {
-    private publish;
-    private applicationStore;
-    observeClass: boolean;
-    onInit(): void;
-    updateVersion(version: string): void;
-}
--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import 'gaea-model';
-import GaeaEditor from './gaea-editor/gaea-editor.component';
-import { PropsDefine as GaeaEditorPropsDefine } from './gaea-editor/gaea-editor.type';
-export { GaeaEditor, GaeaEditorPropsDefine };
-export default GaeaEditor;
