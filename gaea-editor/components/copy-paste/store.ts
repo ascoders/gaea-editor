d1410b985410116a67e44f9a50e52b091856aebc
576625322@qq.com <576625322@qq.com>
2017-03-29 23:01:37 +0800

    empty

--- gaea-editor/components/copy-paste/store.ts
+++
@@ -1,8 +0,0 @@
-import { observable, inject, ViewportStore } from '../../../gaea-editor-manager/gaea-editor-manager'
-
-export default class CopyPasteStore {
-    @inject('ViewportStore') private viewport: ViewportStore
-
-    // 存储粘贴板的存储信息
-    @observable copyComponent?: FitGaea.ViewportComponentFullInfo = null
-}
--- gaea-editor/components/publish/store.ts
+++
@@ -1,8 +0,0 @@
-import { observable, inject, ApplicationStore } from '../../../gaea-editor-manager/gaea-editor-manager'
-
-export default class PublishStore {
-    @inject('ApplicationStore') private application: ApplicationStore
-
-    // 当前版本号
-    @observable currentVersion = '0.0.0'
-}
