2e5841cc057e772d95d59367044091db8402bce5
ascoders <576625322@qq.com>
2017-05-10 11:23:10 +0800

    update

--- src/defines/index.d.ts
+++
@@ -1,2 +0,0 @@
-import "./application.d"
-import "./viewport.d"
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 // tslint:disable-next-line:no-reference
 /// <reference path="../define.d.ts"/>
+// tslint:disable-next-line:no-reference
+/// <reference path="./defines/application.d.ts"/>
+// tslint:disable-next-line:no-reference
+/// <reference path="./defines/viewport.d.ts"/>
 
 export { default as default } from "./gaea-editor.component"
