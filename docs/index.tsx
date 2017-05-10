7a3ef789b5d784f990ef9fe5713c476cf9673b14
ascoders <576625322@qq.com>
2017-05-10 10:51:54 +0800

    deploy 2.0.1

--- docs/index.tsx
+++ docs/index.tsx
@@ -2,10 +2,8 @@ import * as React from "react"
 import * as ReactDOM from "react-dom"
 import GaeaEditor from "../src"
 
-import RemoteComponent from "./components/remote-component/remote-component.component"
-
 ReactDOM.render((
     <div style={{ width: "100vw", height: "100vh" }}>
-        <GaeaEditor componentClasses={[RemoteComponent]} />
+        <GaeaEditor />
     </div>
 ), document.getElementById("react-dom"))
