06f10d6dea7597928c865f5c62fabc4b56987ed3
576625322@qq.com <576625322@qq.com>
2017-07-09 23:20:39 +0800

    deploy 2.0.48

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,9 @@
 // tslint:disable-next-line:no-reference
 /// <reference path="./defines/viewport.d.ts"/>
 
-export { default as default } from "./gaea-editor.component"
+import GaeaEditor from "./gaea-editor.component"
+
+export default GaeaEditor
+
 export { StoreProps } from "./stores/index"
+export { GaeaEditor }
