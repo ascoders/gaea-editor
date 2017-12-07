912c3306656be4c266867501416d79dd77b9032b
ascoders <576625322@qq.com>
2017-12-07 16:50:19 +0800

    fix bug

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,6 @@
 
 import GaeaEditor from "./gaea-editor.component"
 
-import "dob-react-devtools"
-
-import { startDebug } from "dob-react"
-startDebug()
-
 export default GaeaEditor
 
 export { StoreProps } from "./stores/index"
