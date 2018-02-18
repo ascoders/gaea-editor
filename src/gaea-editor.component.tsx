0e52c2a188cac6f40f434db5de9d3f222579c2de
ascoders <576625322@qq.com>
2018-02-18 17:02:48 +0800

    fix: remove webpack special grammar

--- src/gaea-editor.component.tsx
+++ src/gaea-editor.component.tsx
@@ -9,14 +9,64 @@ import { IActionsOrStores, Store } from "./stores"
 // tslint:disable-next-line:no-submodule-imports
 import "antd/dist/antd.css"
 
-// plug-in plugins
-const builtInPlugins: IPlugin[] = []
+import crumbs from "./plugins/crumbs"
+import dragMenu from "./plugins/drag-menu"
+import dragMenuButton from "./plugins/drag-menu-button"
+import mainTool from "./plugins/main-tool"
+import mainToolEditor from "./plugins/main-tool-editor"
+import mainToolEditorEvent from "./plugins/main-tool-editor-event"
+import mainToolEditorEventAction from "./plugins/main-tool-editor-event-action"
+import mainToolEditorEventTrigger from "./plugins/main-tool-editor-event-trigger"
+import mainToolEditorManager from "./plugins/main-tool-editor-manager"
+import mainToolEditorTypeArray from "./plugins/main-tool-editor-type-array"
+import mainToolEditorTypeBoolean from "./plugins/main-tool-editor-type-boolean"
+import mainToolEditorTypeBoxEditor from "./plugins/main-tool-editor-type-box-editor"
+import mainToolEditorTypeColor from "./plugins/main-tool-editor-type-color"
+import mainToolEditorTypeDisplay from "./plugins/main-tool-editor-type-display"
+import mainToolEditorTypeNumber from "./plugins/main-tool-editor-type-number"
+import mainToolEditorTypeObject from "./plugins/main-tool-editor-type-object"
+import mainToolEditorTypeSelect from "./plugins/main-tool-editor-type-select"
+import mainToolEditorTypeString from "./plugins/main-tool-editor-type-string"
+import mainToolEditorVariable from "./plugins/main-tool-editor-variable"
+import mainToolTree from "./plugins/main-tool-tree"
+import page from "./plugins/page"
+import pageAddFolder from "./plugins/page-add-folder"
+import pageAddPage from "./plugins/page-add-page"
+import pageButton from "./plugins/page-button"
+import preview from "./plugins/preview"
+import save from "./plugins/save"
+import viewportGuideline from "./plugins/viewport-guideline"
 
-declare const require: any
-const context = require.context("./plugins", true, /index\.(tsx|js)$/)
-context.keys().forEach((key: string) => {
-    builtInPlugins.push(context(key).default)
-})
+// plug-in plugins
+const builtInPlugins: IPlugin[] = [
+    crumbs,
+    dragMenu,
+    dragMenuButton,
+    mainTool,
+    mainToolEditor,
+    mainToolEditorEvent,
+    mainToolEditorEventAction,
+    mainToolEditorEventTrigger,
+    mainToolEditorManager,
+    mainToolEditorTypeArray,
+    mainToolEditorTypeBoolean,
+    mainToolEditorTypeBoxEditor,
+    mainToolEditorTypeColor,
+    mainToolEditorTypeDisplay,
+    mainToolEditorTypeNumber,
+    mainToolEditorTypeObject,
+    mainToolEditorTypeSelect,
+    mainToolEditorTypeString,
+    mainToolEditorVariable,
+    mainToolTree,
+    page,
+    pageAddFolder,
+    pageAddPage,
+    pageButton,
+    preview,
+    save,
+    viewportGuideline
+]
 
 export default class GaeaEditor extends React.Component<Props, State> {
     public static defaultProps = new Props()
