54f2ccba90503e17c8d06b047034eeb257dee3f0
ascoders <576625322@qq.com>
2019-03-07 17:15:57 +0800

    update demo

--- docs/basic.tsx
+++ docs/basic.tsx
@@ -1,3 +1,4 @@
+import GaeaComponents from 'gaea-basic-components';
 import * as React from 'react';
 import Component from '../src/index';
 
@@ -5,35 +6,24 @@ class Props {}
 
 class State {}
 
+class TestComponent extends React.PureComponent {
+  public static defaultProps = {
+    editSetting: {
+      key: 'aaaa',
+      name: '66666',
+      isContainer: false
+    }
+  };
+  public render() {
+    return <div>123</div>;
+  }
+}
+
 export default class Page extends React.PureComponent<Props, State> {
   public static defaultProps = new Props();
   public state = new State();
 
   public render() {
-    return (
-      <Component
-        // disableBuiltInPlugin={['preview']}
-        // preComponents={[
-        //   {
-        //     gaeaKey: 'gaea-button',
-        //     components: [
-        //       {
-        //         name: 'gaea-button1',
-        //         props: { text: '123' }
-        //       },
-        //       {
-        //         name: 'gaea-button2',
-        //         props: { text: '456' }
-        //       }
-        //     ]
-        //   }
-        // ]}
-        onSave={data => {
-          // tslint:disable-next-line:no-console
-          console.log(JSON.stringify(data, null, 2));
-        }}
-        locale="en"
-      />
-    );
+    return <Component componentClasses={[TestComponent, ...GaeaComponents]} />;
   }
 }
